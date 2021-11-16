5c5481cbc25daed85a710e16beefe2c7502b10fa
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2021-11-16 19:16:59 +0100

    Add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+/*import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDxBDplWVzL5-E4TAJckIP-tAQZSEH9lGo",
+  authDomain: "client-pc.firebaseapp.com",
+  projectId: "client-pc",
+  storageBucket: "client-pc.appspot.com",
+  messagingSenderId: "39195826909",
+  appId: "1:39195826909:web:40bd356ab56991aeac9934",
+  measurementId: "G-E6QZBZ0RFE"
+};
+
+const app = initializeApp(firebaseConfig);
+*/
 @NgModule({
   declarations: [
     AppComponent
