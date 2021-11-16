931cc22e1fd1136b665f00b61304077fca6e5fd3
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2021-11-16 22:57:54 +0100

    Remove useless files + Add Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Client-PC';
+  // To communicate with the back-end
+  // See this link : https://angular.io/guide/http
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-/*import { initializeApp } from "firebase/app";
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDxBDplWVzL5-E4TAJckIP-tAQZSEH9lGo",
-  authDomain: "client-pc.firebaseapp.com",
-  projectId: "client-pc",
-  storageBucket: "client-pc.appspot.com",
-  messagingSenderId: "39195826909",
-  appId: "1:39195826909:web:40bd356ab56991aeac9934",
-  measurementId: "G-E6QZBZ0RFE"
-};
-
-const app = initializeApp(firebaseConfig);
-*/
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
