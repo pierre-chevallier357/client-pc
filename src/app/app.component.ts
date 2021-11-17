3ffb1e2bfc761ee89efacf6cf79295ee150ef224
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2021-11-17 18:57:25 +0100

    Add basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Client-PC';
+  selectedStrategie: string;
+  strategiesList: string[] = [
+    'Donnant donnant',
+    'Donnant donnant/aléatoire',
+    'Donnant pour deux donnants et aléatoire',
+    'Donnant pour deux donnants',
+    'Sondeur naïf',
+    'Sondeur repentant',
+    'Pacificateur naïf',
+    'Vrai pacificateur',
+    'Aléatoire',
+    'Toujours trahir',
+    'Toujours coopérer',
+    'Rancunier',
+    'Pavlov',
+    'Pavlov/aléatoire',
+    'Adaptatif',
+    'Graduel',
+    'Donnant donnant soupçonneux',
+    'Rancunier doux',
+  ];
+  selectedButton: string;
+  player1Score: number = 0;
+  player2Score: number = 0;
+  currentTurn: number = 0;
+  hasAnyPlayerGivenUp: boolean = false;
+
   // To communicate with the back-end
   // See this link : https://angular.io/guide/http
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
